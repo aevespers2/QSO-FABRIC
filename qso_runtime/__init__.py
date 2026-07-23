ee06c2fee7bf5dbdb8ba1ad784bdfbeb6d663498
Jacob Vespers <aevespers2@gmail.com>
2026-07-23 10:01:24 -0700

    export Alistaire field runtime

--- qso_runtime/__init__.py
+++ qso_runtime/__init__.py
@@ -1,5 +1,28 @@
 """Bounded QSO Fabric runtime."""
 
+from .alistaire_field import (
+    CONSTITUTIONAL_VALUES,
+    AlistaireField,
+    AlistaireLimits,
+    AlistaireState,
+    ConstitutionalWeights,
+    Contradiction,
+    FieldDecision,
+    MemoryNode,
+    Possibility,
+)
 from .four_qso_experiment import ExperimentLimits, run_experiment
 
-__all__ = ["ExperimentLimits", "run_experiment"]
+__all__ = [
+    "CONSTITUTIONAL_VALUES",
+    "AlistaireField",
+    "AlistaireLimits",
+    "AlistaireState",
+    "ConstitutionalWeights",
+    "Contradiction",
+    "ExperimentLimits",
+    "FieldDecision",
+    "MemoryNode",
+    "Possibility",
+    "run_experiment",
+]
