82aabfd40dbb498cbbaa5aa5360359028b1fb052
Jacob Vespers <aevespers2@gmail.com>
2026-07-23 21:32:50 -0700

    feat: export QSO glyph language API

--- qso_runtime/__init__.py
+++ qso_runtime/__init__.py
@@ -1,5 +1,14 @@
 """Bounded QSO Fabric runtime."""
 
 from .four_qso_experiment import ExperimentLimits, run_experiment
+from .glyphs import Glyph, GlyphRegistry, Stroke, core_registry, glyph_from_mapping
 
-__all__ = ["ExperimentLimits", "run_experiment"]
+__all__ = [
+    "ExperimentLimits",
+    "Glyph",
+    "GlyphRegistry",
+    "Stroke",
+    "core_registry",
+    "glyph_from_mapping",
+    "run_experiment",
+]
