b21b0d004721cce98f11128bd672e9e3c2704496
Jacob Vespers <aevespers2@gmail.com>
2026-07-24 12:53:08 -0700

    feat: export QSO-HILBERT-1 API

--- qso_runtime/__init__.py
+++ qso_runtime/__init__.py
@@ -2,13 +2,27 @@
 
 from .four_qso_experiment import ExperimentLimits, run_experiment
 from .glyphs import Glyph, GlyphRegistry, Stroke, core_registry, glyph_from_mapping
+from .hilbert_glyphs import (
+    ComplexAmplitude,
+    HilbertScene,
+    HilbertSpace,
+    LinearOperator,
+    StateVector,
+    qubit_scene,
+)
 
 __all__ = [
+    "ComplexAmplitude",
     "ExperimentLimits",
     "Glyph",
     "GlyphRegistry",
+    "HilbertScene",
+    "HilbertSpace",
+    "LinearOperator",
+    "StateVector",
     "Stroke",
     "core_registry",
     "glyph_from_mapping",
+    "qubit_scene",
     "run_experiment",
 ]
